0321df918d759ecb6d02770e8796bcdeff183893
João Luis Moraes <jonmoraesnl@gmail.com>
2022-01-21 01:46:40 -0500

    android splash screen

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Start from './src/screens/Start';
 import Game from './src/screens/Game';
 import Restart from './src/screens/Restart';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createNativeStackNavigator();
 
 export default App => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
